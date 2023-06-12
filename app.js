8471f49bfa72e419ff52e13d8f46f65f693bbb38
Joshua <jwalk99star@gmail.com>
2023-06-12 15:59:53 -0600

    refactored to 2 functions, 1 per team

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 let visitorScore = 0
 let homeScore = 0
 
-function scoreVisitor1() {
-  visitorScore += 1
-  console.log(`visitor score: `, visitorScore);  
-  let visitorElement = document.getElementById('visitor')
-  console.log('visitorElement:', visitorElement);
-  visitorElement.innerText = visitorScore  
-}
+// function scoreVisitor1() {
+//   visitorScore += 1
+//   console.log(`visitor score: `, visitorScore);  
+//   let visitorElement = document.getElementById('visitor')
+//   console.log('visitorElement:', visitorElement);
+//   visitorElement.innerText = visitorScore  
+// }
 
-function scoreVisitor3() {
-  visitorScore += 3
+function scoreVisitor(num) {
+  visitorScore += num
   console.log(`visitor score: `, visitorScore);
   let visitorElement = document.getElementById('visitor')
-  console.log('visitorElement:', visitorElement);
+  // console.log('visitorElement:', visitorElement);
   visitorElement.innerText = visitorScore
 }
 
 
-function scoreHome1() {
-  homeScore += 1
+
+function scoreHome(num) {
+  homeScore += num
   console.log(`home score: `, homeScore);  
   let homeElement = document.getElementById('home')
-  console.log('homeElement:', homeElement);
+  // console.log('homeElement:', homeElement);
   homeElement.innerText = homeScore  
 }
 
-function scoreHome3() {
-  homeScore += 3
-  console.log(`home score: `, homeScore);
-  let homeElement = document.getElementById('home')
-  console.log('homeElement:', homeElement);
-  homeElement.innerText = homeScore
-}
+// function scoreHome3() {
+//   homeScore += 3
+//   console.log(`home score: `, homeScore);
+//   let homeElement = document.getElementById('home')
+//   console.log('homeElement:', homeElement);
+//   homeElement.innerText = homeScore
+// }

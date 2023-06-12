ff7103568b89622a08d0f311d11cf210479e95ec
Joshua <jwalk99star@gmail.com>
2023-06-12 15:21:08 -0600

    initial ugly 1 team push

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 let visitorScore = 0
+let homeScore = 0
 
 function scoreVisitor1() {
   visitorScore += 1
-  console.log(visitorScore);
-  
-  
+  console.log(`visitor score: `, visitorScore);  
   let visitorElement = document.getElementById('visitor')
   console.log('visitorElement:', visitorElement);
   visitorElement.innerText = visitorScore
   
+}
+
+function scoreVisitor3() {
+  visitorScore += 3
+  console.log(`visitor score: `, visitorScore);
+  let visitorElement = document.getElementById('visitor')
+  console.log('visitorElement:', visitorElement);
+  visitorElement.innerText = visitorScore
 }

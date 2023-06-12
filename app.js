de18db63d0a36e9adf865aa1dafd2d9db3a56c14
Joshua <jwalk99star@gmail.com>
2023-06-12 16:55:09 -0600

    almost EoD.4 goals done. 2 left.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// ✅make 4 buttons, 2 for each team. A +1 point, and a +3 points.
+// ✅write the 'functions' to add points to correct team when buttons are clicked
+// ✅make 2 scoreboards, 1 for each team that displays those growing totals
+// ✅refactor down to 2 functions..?
+// ☑️refactor down to 1 function..? *NOTE: Jeremy said that may be tricky at this point. multiple parameters and arguments.
+//  I'll hold off on that one.
+// ☑️make a reset button/function to set scores back to zero and start over
+// ☑️make it look cool (figma for reference)
+
+
+
 let visitorScore = 0
 let homeScore = 0
 
@@ -17,8 +28,6 @@ function scoreVisitor(num) {
   visitorElement.innerText = visitorScore
 }
 
-
-
 function scoreHome(num) {
   homeScore += num
   console.log(`home score: `, homeScore);  
@@ -28,9 +37,20 @@ function scoreHome(num) {
 }
 
 // function scoreHome3() {
-//   homeScore += 3
-//   console.log(`home score: `, homeScore);
-//   let homeElement = document.getElementById('home')
-//   console.log('homeElement:', homeElement);
-//   homeElement.innerText = homeScore
-// }
+  //   homeScore += 3
+  //   console.log(`home score: `, homeScore);
+  //   let homeElement = document.getElementById('home')
+  //   console.log('homeElement:', homeElement);
+  //   homeElement.innerText = homeScore
+  // }
+  
+  function reset() {
+    let visitorScore = 0
+    let visitorElement = document.getElementById('visitor')
+    visitorElement.innerText = visitorScore
+    
+    let homeScore = 0
+    console.log('reset button');
+    let homeElement = document.getElementById('home')
+    homeElement.innerText = homeScore  
+}
